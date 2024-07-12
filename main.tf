fac41197bf7f615ae9a8e84fd1f05ea4000aa82f
Demo220202 <158024374+Demo220202@users.noreply.github.com>
2024-07-12 13:36:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,13 @@ variable "lambda_name" {
 variable "environments" {
   description = "The list of environments for which the Lambda functions will be created"
   type        = list(string)
-  default     = ["dev", "qa", "beta", "prod", "dr"]
+  default     = ["dev", "qa", "beta", "prod"]
+}
+
+variable "environment_dr" {
+  description = "DR Env"
+  type        = string
+  default     = "dr"
 }
 
 variable "existing_iam_role_name" {
@@ -29,11 +35,13 @@ variable "default_region" {
 provider "aws" {
   alias  = "default"
   region = var.default_region
+  profile = "Aditya-demo"
 }
 
 provider "aws" {
   alias  = "dr"
   region = var.dr_region
+  profile = "Aditya-demo"
 }
 
 data "aws_iam_role" "existing_role" {
@@ -43,10 +51,30 @@ data "aws_iam_role" "existing_role" {
 
 resource "aws_lambda_function" "zen_lambda" {
   count = length(var.environments)
-  
-  provider = var.environments[count.index] == "dr" ? aws.dr : aws.default
+
+  provider = aws.default
 
   function_name = "Zen-${var.environments[count.index]}-${var.lambda_name}"
+  role          = data.aws_iam_role.existing_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+
+  # Source code for the Lambda function from a local zip file
+  filename = "lambda_function_payload.zip"
+
+  # Optionally, you can add environment variables
+  # environment {
+  #   variables = {
+  #     ENV = var.environments[count.index]
+  #   }
+  # }
+}
+
+resource "aws_lambda_function" "zen_lambda_dr" {
+  
+  provider = aws.dr
+
+  function_name = "Zen-${var.environment_dr}-${var.lambda_name}"
   role          = data.aws_iam_role.existing_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
